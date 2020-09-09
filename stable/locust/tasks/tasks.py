d9138c1141f439a3d755a925702da4b8dc6e3e05
Ganesh Bhat <gbhat@synamedia.com>
2020-09-09 14:18:30 -0400

    gb locust changes

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -1,11 +1,21 @@
-from locust import HttpLocust, TaskSet, task
+import time, json
+from locust import HttpUser, task, between
 
-class ElbTasks(TaskSet):
-  @task
-  def status(self):
-      self.client.get("/status")
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
 
-class ElbWarmer(HttpLocust):
-  task_set = ElbTasks
-  min_wait = 1000
-  max_wait = 3000
+    @task
+    def post_telemetry(self):
+        # for running in dokcer
+        # regex = json.loads(open("/mnt/locust/files/rtl_json.txt", "r").read())
+        # for running locally
+        regex = json.loads(open("files/rtl_json.txt", "r").read())
+        self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
+
+    @task
+    def post_log(self):
+        # for running in dokcer
+        # log = {"filename": open("/mnt/locust/files/B8:27:EB:1A:FD:FF-Logs-06-16-20-06-37PM.tgz", "rb")}
+        # for running locally
+        log = {"filename": open("files/B8:27:EB:1A:FD:FF-Logs-06-16-20-06-37PM.tgz", "rb")}
+        self.client.post("/erdk/upload/device/log", files=log)
