60246d7cb1d410b2ea7d931d3128e39513f854df
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 17:44:01 -0400

    enabling log upload

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -6,12 +6,11 @@ class ElbTasks(TaskSet):
   def post_telemetry(self):
       telemetry = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
       self.client.post("/erdk/upload/device/telemetry", data=json.dumps(telemetry))
-    #   self.client.post("/erdk/upload/device/telemetry", data=json.dumps({'hello': 'world'}))
 
-#   @task
-#   def post_log(self):
-#       log = {"filename": open("/locust-tasks/log.tgz", "rb")}
-#       self.client.post("/erdk/upload/device/log", files=log)
+  @task
+  def post_log(self):
+      log = {"filename": open("/locust-tasks/log.tgz", "rb")}
+      self.client.post("/erdk/upload/device/log", files=log)
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
