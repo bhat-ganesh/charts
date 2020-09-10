a9f5cdf6824df20baa02a6721c4361d72cae72be
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 16:45:09 -0400

    fix again

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -1,17 +1,20 @@
 from locust import HttpLocust, TaskSet, task
 
 class ElbTasks(TaskSet):
-    @task
-    import json
-    def post_telemetry(self):
-        regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
-        self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
+  @task
+  def status(self):
+      self.client.get("/status")
+    # @task
+    # import json
+    # def post_telemetry(self):
+    #     regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
+    #     self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
 
-    @task
-    import json
-    def post_log(self):
-        log = {"filename": open("/locust-tasks/log.tgz", "rb")}
-        self.client.post("/erdk/upload/device/log", files=log)
+    # @task
+    # import json
+    # def post_log(self):
+    #     log = {"filename": open("/locust-tasks/log.tgz", "rb")}
+    #     self.client.post("/erdk/upload/device/log", files=log)
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
