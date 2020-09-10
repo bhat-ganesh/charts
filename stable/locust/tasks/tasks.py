da6669986f049016205e93e51cd7a8f53397d881
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 16:54:53 -0400

    syntax fix

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -1,17 +1,16 @@
 from locust import HttpLocust, TaskSet, task
+import json
 
 class ElbTasks(TaskSet):
 #   @task
 #   def status(self):
 #       self.client.get("/status")
   @task
-  import json
   def post_telemetry(self):
       regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
       self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
 
   @task
-  import json
   def post_log(self):
       log = {"filename": open("/locust-tasks/log.tgz", "rb")}
       self.client.post("/erdk/upload/device/log", files=log)
