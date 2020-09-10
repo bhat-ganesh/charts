5ce762685fbf9a79f8ef86abe280cd7b4d567a01
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 16:27:00 -0400

    updates for script

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -1,9 +1,16 @@
+import time, json
 from locust import HttpLocust, TaskSet, task
 
 class ElbTasks(TaskSet):
-  @task
-  def status(self):
-      self.client.get("/status")
+    @task
+    def post_telemetry(self):
+        regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
+        self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
+
+    @task
+    def post_log(self):
+        log = {"filename": open("/locust-tasks/log.tgz", "rb")}
+        self.client.post("/erdk/upload/device/log", files=log)
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
