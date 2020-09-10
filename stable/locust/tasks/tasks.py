aa99fd17161ee403ffb906ab8eebadc9169463e3
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 16:50:14 -0400

    changing task file

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -2,19 +2,19 @@ from locust import HttpLocust, TaskSet, task
 
 class ElbTasks(TaskSet):
   @task
-  def status(self):
-      self.client.get("/status")
-    # @task
-    # import json
-    # def post_telemetry(self):
-    #     regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
-    #     self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
+#   def status(self):
+#       self.client.get("/status")
+  @task
+  import json
+  def post_telemetry(self):
+      regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
+      self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
 
-    # @task
-    # import json
-    # def post_log(self):
-    #     log = {"filename": open("/locust-tasks/log.tgz", "rb")}
-    #     self.client.post("/erdk/upload/device/log", files=log)
+  @task
+  import json
+  def post_log(self):
+      log = {"filename": open("/locust-tasks/log.tgz", "rb")}
+      self.client.post("/erdk/upload/device/log", files=log)
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
