15872753a75cb8c6fbecfad278559dcb9841bd7c
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 16:56:47 -0400

    test

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -1,5 +1,5 @@
 from locust import HttpLocust, TaskSet, task
-import json
+# import json
 
 class ElbTasks(TaskSet):
 #   @task
@@ -7,13 +7,14 @@ class ElbTasks(TaskSet):
 #       self.client.get("/status")
   @task
   def post_telemetry(self):
-      regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
-      self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
+    #   regex = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
+    #   self.client.post("/erdk/upload/device/telemetry", data=json.dumps(regex))
+      self.client.post("/erdk/upload/device/telemetry", data={})
 
-  @task
-  def post_log(self):
-      log = {"filename": open("/locust-tasks/log.tgz", "rb")}
-      self.client.post("/erdk/upload/device/log", files=log)
+#   @task
+#   def post_log(self):
+#       log = {"filename": open("/locust-tasks/log.tgz", "rb")}
+#       self.client.post("/erdk/upload/device/log", files=log)
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
