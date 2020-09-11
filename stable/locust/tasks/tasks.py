59dab0e70c23c1685a789e9b76d9be2227d84765
Ganesh Bhat <gbhat@synamedia.com>
2020-09-10 22:08:21 -0400

    back to writing file error

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -13,16 +13,17 @@ class ElbTasks(TaskSet):
       log_txt = fl.read().replace('\n', '')
       fl.close()
 
-    #   fb = open("/locust-tasks/B8:27:EB:1A:FD:FF-Logs-06-16-20-06-37PM.tgz", "wb")
-    #   fb.write(log_txt.decode('base64'))
-    #   fb.close()
+      fb = open("/locust-tasks/B8:27:EB:1A:FD:FF-Logs-06-16-20-06-37PM.tgz", "wb")
+      fb.write(log_txt.decode('base64'))
+      fb.close()
 
-    #   log = {"filename": open("/locust-tasks/B8:27:EB:1A:FD:FF-Logs-06-16-20-06-37PM.tgz", "rb")}
+      log = {"filename": open("/locust-tasks/B8:27:EB:1A:FD:FF-Logs-06-16-20-06-37PM.tgz", "rb")}
 
     #   body_file = io.BytesIO(base64.b64decode(log_txt))
     #   log = {"filename": body_file.getbuffer().nbytes}
 
-      log = {"filename": log_txt.decode('base64').getbuffer().nbytes}
+    #   log = {"filename": log_txt.decode('base64').getbuffer().nbytes}
+
       self.client.post("/erdk/upload/device/log", files=log)
 
 class ElbWarmer(HttpLocust):
