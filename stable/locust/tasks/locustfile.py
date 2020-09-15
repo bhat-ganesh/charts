21df38fbd4aa236233645aeee215326325dc6fc8
Ganesh Bhat <gbhat@synamedia.com>
2020-09-15 10:06:30 -0400

    reverting to greenbirdit/locust v0.14.5

--- stable/locust/tasks/locustfile.py
+++ stable/locust/tasks/locustfile.py
@@ -1,12 +1,14 @@
-from locust import HttpUser, task, between
+#############################################################################################################################################################
+
+from locust import HttpLocust, TaskSet, task
 from time import gmtime, strftime
 import json, base64
 
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 2)
-
-    @task
-    def post_telemetry(self):
+class UploadEndpointTasks(TaskSet):
+  @task
+  def post_telemetry(self):
+      # telemetry = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
+      # self.client.post("/erdk/upload/device/telemetry", data=json.dumps(telemetry))
       ts = strftime("%Y-%m-%d %H:%M:%S", gmtime())
       self.client.post("/erdk/upload/device/telemetry", data=json.dumps({"searchResult":[{"Profile":"RDKB"},
                                                                                          {"mac":"FF:FF:FF:FF:FF:FF"},
@@ -17,8 +19,8 @@ class QuickstartUser(HttpUser):
                                                                                          {"Version":"rdkb-generic-broadband-image_default_20200619132645"}
                                                                                          ,{"Time":ts}]}))
 
-    @task
-    def post_log(self):
+  @task
+  def post_log(self):
       fl = open('/locust-tasks/log.txt', 'r')
       log_txt = fl.read().replace('\n', '')
       fl.close()
@@ -30,16 +32,22 @@ class QuickstartUser(HttpUser):
       log = {"filename": open("/tmp/FF:FF:FF:FF:FF:FF-Logs-09-15-20-01-00PM.tgz", "rb")}
       self.client.post("/erdk/upload/device/log", files=log)
 
+class UploadEndpointTest(HttpLocust):
+  task_set = UploadEndpointTasks
+  min_wait = 1000
+  max_wait = 3000
+
 #############################################################################################################################################################
-# from locust import HttpLocust, TaskSet, task
+
+# from locust import HttpUser, task, between
 # from time import gmtime, strftime
 # import json, base64
 
-# class UploadEndpointTasks(TaskSet):
-#   @task
-#   def post_telemetry(self):
-#       # telemetry = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
-#       # self.client.post("/erdk/upload/device/telemetry", data=json.dumps(telemetry))
+# class QuickstartUser(HttpUser):
+#     wait_time = between(1, 2)
+
+#     @task
+#     def post_telemetry(self):
 #       ts = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 #       self.client.post("/erdk/upload/device/telemetry", data=json.dumps({"searchResult":[{"Profile":"RDKB"},
 #                                                                                          {"mac":"FF:FF:FF:FF:FF:FF"},
@@ -50,8 +58,8 @@ class QuickstartUser(HttpUser):
 #                                                                                          {"Version":"rdkb-generic-broadband-image_default_20200619132645"}
 #                                                                                          ,{"Time":ts}]}))
 
-#   @task
-#   def post_log(self):
+#     @task
+#     def post_log(self):
 #       fl = open('/locust-tasks/log.txt', 'r')
 #       log_txt = fl.read().replace('\n', '')
 #       fl.close()
@@ -63,8 +71,4 @@ class QuickstartUser(HttpUser):
 #       log = {"filename": open("/tmp/FF:FF:FF:FF:FF:FF-Logs-09-15-20-01-00PM.tgz", "rb")}
 #       self.client.post("/erdk/upload/device/log", files=log)
 
-# class UploadEndpointTest(HttpLocust):
-#   task_set = UploadEndpointTasks
-#   min_wait = 1000
-#   max_wait = 3000
 #############################################################################################################################################################
