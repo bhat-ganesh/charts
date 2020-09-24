2574f34e1cfb7cd153e64f04fe9345a385ec37ae
Ganesh Bhat <gbhat@synamedia.com>
2020-09-24 12:11:07 -0400

    randomize

--- stable/locust/tasks/locustfile.py
+++ stable/locust/tasks/locustfile.py
@@ -6,24 +6,27 @@ import json, base64, os, tarfile, random
 
 class LogTelemetryUploadTest(HttpUser):
     wait_time = between(3, 5)
-    cur_time = gmtime()
-    telemetry_ts = strftime("%Y-%m-%d %H:%M:%S", cur_time)
-    log_ts = strftime("%m-%d-%y-%H-%M-%S-", cur_time)
+
+    @task
+    ts = strftime("%m-%d-%y-%H-%M-%S-", gmtime())
     random = [ 0xB8, 0x27, 0xEB, random.randint(0x00, 0x7f), random.randint(0x00, 0xff), random.randint(0x00, 0xff) ]
     mac = ':'.join(map(lambda x: "%02x" % x, random))
 
-    @task
     def post_telemetry(self):
       self.client.post("/erdk/upload/device/telemetry", data=json.dumps({"searchResult":[{"Profile":"RDKB"},
-                                                                                         {"mac":self.mac},
+                                                                                         {"mac":mac},
                                                                                          {"erouterIpv4":"192.168.2.36"},
                                                                                          {"erouterIpv6":"null"},
                                                                                          {"PartnerId":"RDKM"},
                                                                                          {"AccountId":"Unknown"},
                                                                                          {"Version":"rdkb-generic-broadband-image_default_20200619132645"},
-                                                                                         {"Time":self.telemetry_ts}]}))
+                                                                                         {"Time":ts}]}))
 
     @task
+    ts = strftime("%m-%d-%y-%H-%M-%S-", gmtime())
+    random = [ 0xB8, 0x27, 0xEB, random.randint(0x00, 0x7f), random.randint(0x00, 0xff), random.randint(0x00, 0xff) ]
+    mac = ':'.join(map(lambda x: "%02x" % x, random))
+
     def post_log(self):
       log_path = "/tmp/log/"
 
@@ -43,11 +46,11 @@ class LogTelemetryUploadTest(HttpUser):
 
       os.remove(log_path + "log.tgz")
 
-      with tarfile.open("/tmp/" + self.mac + "-Logs.tgz", "w:gz") as tar:
+      with tarfile.open("/tmp/" + mac + "-Logs.tgz", "w:gz") as tar:
           for filename in os.listdir(log_path):
-              tar.add(log_path + filename, arcname=self.log_ts + filename)
+              tar.add(log_path + filename, arcname=ts + filename)
 
-      log = {"filename": open("/tmp/" + self.mac + "-Logs.tgz", "rb")}
+      log = {"filename": open("/tmp/" + mac + "-Logs.tgz", "rb")}
       self.client.post("/erdk/upload/device/log", files=log)
 
 
