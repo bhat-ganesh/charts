e68ab680ff6581ae888b487c0fa8d314d8ffe652
Ganesh Bhat <gbhat@synamedia.com>
2020-09-24 12:48:33 -0400

    cleanup

--- stable/locust/tasks/locustfile.py
+++ stable/locust/tasks/locustfile.py
@@ -1,5 +1,3 @@
-#############################################################################################################################################################
-
 from locust import HttpUser, task, between
 from time import gmtime, strftime
 import json, base64, os, tarfile, random
@@ -13,7 +11,7 @@ class LogTelemetryUploadTest(HttpUser):
       mac_arr = [ 0xB8, 0x27, 0xEB, random.randint(0x00, 0x7f), random.randint(0x00, 0xff), random.randint(0x00, 0xff) ]
       mac = ':'.join(map(lambda x: "%02x" % x, mac_arr))
 
-      self.client.post("/erdk/upload/device/telemetry", data=json.dumps({"searchResult":[{"Profile":"RDKB"},
+      self.client.post("/upload/device/telemetry", data=json.dumps({"searchResult":[{"Profile":"RDKB"},
                                                                                          {"mac":mac},
                                                                                          {"erouterIpv4":"192.168.2.36"},
                                                                                          {"erouterIpv6":"null"},
@@ -51,56 +49,4 @@ class LogTelemetryUploadTest(HttpUser):
               tar.add(log_path + filename, arcname=ts + filename)
 
       log = {"filename": open("/tmp/" + mac + "-Logs.tgz", "rb")}
-      self.client.post("/erdk/upload/device/log", files=log)
-
-
-
-      # old code, same file without ts --> start
-      # fb = open(log_path + "FF:FF:FF:FF:FF:FF-Logs.tgz", "wb")
-      # fb.write(base64.b64decode(log_txt))
-      # fb.close()
-
-      # log = {"filename": open(log_path + "FF:FF:FF:FF:FF:FF-Logs.tgz", "rb")}
-      # self.client.post("/erdk/upload/device/log", files=log)
-      # old code, same file without ts --> end
-
-#############################################################################################################################################################
-
-# from locust import HttpLocust, TaskSet, task
-# from time import gmtime, strftime
-# import json, base64
-
-# class UploadEndpointTasks(TaskSet):
-#   @task
-#   def post_telemetry(self):
-#       # telemetry = json.loads(open("/locust-tasks/rtl_json.txt", "r").read())
-#       # self.client.post("/erdk/upload/device/telemetry", data=json.dumps(telemetry))
-#       ts = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-#       self.client.post("/erdk/upload/device/telemetry", data=json.dumps({"searchResult":[{"Profile":"RDKB"},
-#                                                                                          {"mac":"FF:FF:FF:FF:FF:FF"},
-#                                                                                          {"erouterIpv4":"192.168.2.36"},
-#                                                                                          {"erouterIpv6":"null"},
-#                                                                                          {"PartnerId":"RDKM"},
-#                                                                                          {"AccountId":"Unknown"},
-#                                                                                          {"Version":"rdkb-generic-broadband-image_default_20200619132645"}
-#                                                                                          ,{"Time":ts}]}))
-
-#   @task
-#   def post_log(self):
-#       fl = open('/locust-tasks/log.txt', 'r')
-#       log_txt = fl.read().replace('\n', '')
-#       fl.close()
-
-#       fb = open("/tmp/FF:FF:FF:FF:FF:FF-Logs.tgz", "wb")
-#       fb.write(base64.b64decode(log_txt))
-#       fb.close()
-
-#       log = {"filename": open("/tmp/FF:FF:FF:FF:FF:FF-Logs.tgz", "rb")}
-#       self.client.post("/erdk/upload/device/log", files=log)
-
-# class UploadEndpointTest(HttpLocust):
-#   task_set = UploadEndpointTasks
-#   min_wait = 1000
-#   max_wait = 3000
-
-#############################################################################################################################################################
+      self.client.post("/upload/device/log", files=log)
